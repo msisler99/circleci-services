8a63b7a135666ac9e230c60e987102cf93c31b02
msisler99 <32968954+msisler99@users.noreply.github.com>
2018-12-07 10:45:15 -0800

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "ttg-tf-test-bucket"
-    dynamodb_table = "terraform-state-lock-dev"
-    key    = "test2-terraform.tfstate"
-    region =  "us-west-2"
-  }
-}
