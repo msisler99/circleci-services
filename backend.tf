beee4d689ac248582a3aca855d831d7948483ae3
msisler99 <32968954+msisler99@users.noreply.github.com>
2018-12-14 11:39:49 -0800

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "ttg-tf-test-bucket2"
     dynamodb_table = "terraform-state-lock-dev"
-    key    = "GENERIC/terraform.tfstate"
+    key    = "CA-MMIS/terraform.tfstate"
     region = "us-west-2"
   }
 }
